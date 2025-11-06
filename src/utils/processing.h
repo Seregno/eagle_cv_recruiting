39db2f4b2d619d5a3dbed29b1f1f3831be5f183d
Alessandro Bettale <72103533+Seregno@users.noreply.github.com>
2025-11-06 23:17:49 +0100

    Update processing.h
    Improved comments

--- src/utils/processing.h
+++ src/utils/processing.h
@@ -7,29 +7,21 @@
 #include <cmath>
 #include "constants.h"
 
-// ─────────────────────────────────────────────
 //  Environment and Debug Utilities
-// ─────────────────────────────────────────────
 void printEnvironmentInfo();
 void printImg(cv::Mat to_print, std::string msg);
 
-// ─────────────────────────────────────────────
 //  Image Processing
-// ─────────────────────────────────────────────
 void processConeMask(cv::Mat& mask, 
                      const cv::Mat& kernel_dilate, 
                      const cv::Mat& kernel_morph_open, 
                      const cv::Mat& kernel_morph_close);
 
-// ─────────────────────────────────────────────
 //  Geometry & Math Utilities
-// ─────────────────────────────────────────────
 double point_distance(const cv::Point& a, const cv::Point& b);
 cv::Point median_point(const cv::Point& a, const cv::Point& b);
 
-// ─────────────────────────────────────────────
-//  Cones and Circuit Utilities
-// ─────────────────────────────────────────────
+//  Red cones updates and circuit APIs for detecting, sorting and drawing it
 void assign_red_cone(cv::Point& red_cone, const cv::Point& new_cone, const int selected_cone);
 
 cv::Point get_circuit_point(const cv::Point& current_cone, 
@@ -40,9 +32,7 @@ void sortCircuitPoints(const cv::Point& starting_point,
 
 void drawCircuit(cv::Mat& image, const std::vector<cv::Point>& circuit_points);
 
-// ─────────────────────────────────────────────
 //  Pose Estimation
-// ─────────────────────────────────────────────
 void pose_estimation();
 
-#endif // PROCESSING_H
+#endif 
